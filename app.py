6ee0b838df3fd4c79ec30c23c3038d8b9746ebd3
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2022-08-16 17:08:17 +0100

    Ask MyPy to very kindly go away

--- app.py
+++ app.py
@@ -1,59 +1,59 @@
-# from retrofit import (
-#     Retrofit,
-#     get,
-#     post,
-#     Headers,
-#     Header,
-#     Query,
-#     Path,
-#     Queries,
-#     # Body,
-#     headers,
-#     query_params,
-# )
-# from retrofit.converters import IdentityConverter, IdentityResolver
-# from typing import Any, Dict, List, Protocol, Optional, Set
+from retrofit import (
+    Retrofit,
+    get,
+    post,
+    Headers,
+    Header,
+    Query,
+    Path,
+    Queries,
+    # Body,
+    headers,
+    query_params,
+)
+from retrofit.converters import IdentityConverter, IdentityResolver
+from typing import Any, Dict, List, Protocol, Optional, Set
 
 
-# class HttpBinService(Protocol):
-#     ## Request Inspection
-#     # @get("ip")
-#     # def ip(self) -> dict:
-#     #     ...
+class HttpBinService(Protocol):
+    ## Request Inspection
+    # @get("ip")
+    # def ip(self) -> dict:
+    #     ...
 
-#     # @get("headers")
-#     # def headers(self, headers: dict = Headers(default_factory=lambda: {"x-foo": "bar"})) -> dict:
-#     #     ...
+    # @get("headers")
+    # def headers(self, headers: dict = Headers(default_factory=lambda: {"x-foo": "bar"})) -> dict:
+    #     ...
 
-#     @get("user-agent")
-#     def user_agent(self, user_agent: str = Header()) -> dict:
-#         ...
+    @get("user-agent")
+    def user_agent(self, user_agent: str = Header()) -> dict:
+        ...
 
-#     # @get
-#     # def redirect_to(
-#     #     url: str = Query(), status_code: Optional[int] = Query(default=None)
-#     # ) -> dict:
-#     #     ...
+    # @get
+    # def redirect_to(
+    #     url: str = Query(), status_code: Optional[int] = Query(default=None)
+    # ) -> dict:
+    #     ...
 
-#     # @get("status/{code}")
-#     # def status(code: int = Path()) -> dict:
-#     #     ...
+    # @get("status/{code}")
+    # def status(code: int = Path()) -> dict:
+    #     ...
 
-#     # @get
-#     # def get(params: dict = QueryDict(default_factory=dict)) -> dict:
-#     #     ...
+    # @get
+    # def get(params: dict = QueryDict(default_factory=dict)) -> dict:
+    #     ...
 
-#     # @headers({"User-Agent": "robototron", "X-Who-Am-I": "Sam"})
-#     # @query_params({"foo": "bar", "cat": "dog"})
-#     # @post
-#     # def post(message: str = Body()) -> dict:
-#     #     ...
+    # @headers({"User-Agent": "robototron", "X-Who-Am-I": "Sam"})
+    # @query_params({"foo": "bar", "cat": "dog"})
+    # @post
+    # def post(message: str = Body()) -> dict:
+    #     ...
 
 
-# retrofit: Retrofit = Retrofit(
-#     base_url="https://httpbin.org/",
-#     resolver=IdentityResolver(),
-#     converter=IdentityConverter(),
-# )
+retrofit: Retrofit = Retrofit(
+    base_url="https://httpbin.org/",
+    resolver=IdentityResolver(),
+    converter=IdentityConverter(),
+)
 
-# httpbin: HttpBinService = retrofit.create(HttpBinService)
+httpbin: HttpBinService = retrofit.create(HttpBinService) # type: ignore
