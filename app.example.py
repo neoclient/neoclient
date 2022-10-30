8b0ade755a87f3c0c2e95ad469b548390188b719
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2022-10-30 17:07:54 +0000

    remove dev artifacts

--- app.example.py
+++
@@ -1,7 +0,0 @@
-from fastclient import FastClient
-
-app = FastClient("https://httpbin.org/")
-
-@app.get("/anything/{path}")
-def anything(path: str):
-    ...
--- tst.composer-funcs.py
+++
@@ -1,6 +0,0 @@
-from fastclient.composition.composers import compose_query_param
-from fastclient.parameters import QueryParameter
-
-d = compose_query_param(QueryParameter(alias="foo"), 123)
-# d = compose_query_param(Query(), 123) # missing alias
-# d = compose_query_param(Query(alias="foo"), None) # ignorable
