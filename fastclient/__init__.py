4d3c4182a6e899c083ca373d7f317b8fb0dcbf13
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2022-11-05 15:55:12 +0000

    Disable logging by default

--- fastclient/__init__.py
+++ fastclient/__init__.py
@@ -1,5 +1,54 @@
 __version__: str = "0.1.3"
 
+__all__ = [
+    # .client
+    "FastClient",
+    # .composition.decorators
+    "content",
+    "cookie",
+    "cookies",
+    "data",
+    "files",
+    "header",
+    "headers",
+    "json",
+    "path",
+    "path_params",
+    "query",
+    "query_params",
+    "timeout",
+    # .methods
+    "delete",
+    "get",
+    "head",
+    "options",
+    "patch",
+    "post",
+    "put",
+    "request",
+    # .models
+    "Client",
+    # .parameter_functions
+    "URL",
+    "Body",
+    "Cookie",
+    "Cookies",
+    "Depends",
+    "Header",
+    "Headers",
+    "Path",
+    "Paths",
+    "Queries",
+    "Query",
+    "Request",
+    "Response",
+    "StatusCode",
+]
+
+from loguru import logger
+
+logger.disable(__name__)
+
 from .client import FastClient
 from .composition.decorators import (
     content,
@@ -16,7 +65,16 @@ from .composition.decorators import (
     query_params,
     timeout,
 )
-from .methods import delete, get, head, options, patch, post, put, request
+from .methods import (
+    delete,
+    get,
+    head,
+    options,
+    patch,
+    post,
+    put,
+    request,
+)
 from .models import Client
 from .parameter_functions import (
     URL,
