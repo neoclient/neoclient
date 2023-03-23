6867ba952ade2375bd93185b70296c8e5089c425
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2023-03-23 22:56:37 +0000

    [#91] test_dependencies: add test_cookies, test_elapsed

--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -1,6 +1,8 @@
+import datetime
 from http import HTTPStatus
+from typing import Mapping, MutableSequence, Tuple
 
-from httpx import Cookies, Response
+from httpx import Cookies, Request, Response
 
 
 def test_charset_encoding() -> None:
@@ -22,6 +24,39 @@ def test_content() -> None:
     assert response.content == content
 
 
+def test_cookies() -> None:
+    domain: str = "foo.bar"
+    path: str = "/"
+    cookies: Mapping[str, str] = {"x-foo": "foo", "x-bar": "bar"}
+
+    response_headers: MutableSequence[Tuple[str, str]] = []
+    expected_cookies: Cookies = Cookies()
+
+    cookie_name: str
+    cookie_value: str
+    for cookie_name, cookie_value in cookies.items():
+        response_headers.append(("Set-Cookie", f"{cookie_name}={cookie_value}"))
+        expected_cookies.set(cookie_name, cookie_value, domain=domain, path=path)
+
+    response: Response = Response(
+        HTTPStatus.OK,
+        request=Request("GET", f"https://{domain}/"),
+        headers=response_headers,
+    )
+
+    assert response.cookies == expected_cookies
+
+
+def test_elapsed() -> None:
+    elapsed: datetime.timedelta = datetime.timedelta(seconds=32)
+
+    response: Response = Response(HTTPStatus.OK)
+
+    response.elapsed = elapsed
+
+    assert response.elapsed == elapsed
+
+
 def test_encoding() -> None:
     encoding: str = "foo"
 
@@ -30,8 +65,117 @@ def test_encoding() -> None:
     assert response.encoding == encoding
 
 
-def test_cookies() -> None:
-    cookies: Cookies = Cookies({"x-foo": "bar"})
+def test_has_redirect_location() -> None:
+    ...
 
-    # TODO!
-    assert 1 == 2
+
+def test_headers() -> None:
+    ...
+
+
+def test_history() -> None:
+    ...
+
+
+def test_http_version() -> None:
+    ...
+
+
+def test_is_client_error() -> None:
+    ...
+
+
+def test_is_closed() -> None:
+    ...
+
+
+def test_is_error() -> None:
+    ...
+
+
+def test_is_informational() -> None:
+    ...
+
+
+def test_is_redirect() -> None:
+    ...
+
+
+def test_is_server_error() -> None:
+    ...
+
+
+def test_is_stream_consumed() -> None:
+    ...
+
+
+def test_is_success() -> None:
+    ...
+
+
+def test_json() -> None:
+    ...
+
+
+def test_links() -> None:
+    ...
+
+
+def test_next_request() -> None:
+    ...
+
+
+def test_num_bytes_downloaded() -> None:
+    ...
+
+
+def test_reason_phrase() -> None:
+    ...
+
+
+def test_request() -> None:
+    ...
+
+
+def test_response() -> None:
+    ...
+
+
+def test_status_code() -> None:
+    ...
+
+
+def test_stream() -> None:
+    ...
+
+
+def test_text() -> None:
+    ...
+
+
+def test_url() -> None:
+    ...
+
+
+def test_request_content() -> None:
+    ...
+
+
+def test_request_headers() -> None:
+    ...
+
+
+def test_request_method() -> None:
+    ...
+
+
+def test_request_params() -> None:
+    ...
+
+
+def test_request_stream() -> None:
+    ...
+
+
+def test_request_url() -> None:
+    ...
