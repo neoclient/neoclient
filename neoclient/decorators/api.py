760510d7caf9c0d413d937461a55328992a48f75
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2023-08-02 23:53:57 +0100

    Implement SupportsResolveRequest and SupportsResolveResponse and use for resolvers

--- neoclient/decorators/api.py
+++ neoclient/decorators/api.py
@@ -5,7 +5,7 @@ from ..errors import CompositionError
 from ..models import ClientOptions, PreRequest
 from ..operation import get_operation
 from ..service import Service
-from ..typing import Decorator, SupportsClientConsumer, SupportsRequestConsumer
+from ..typing import Decorator, SupportsConsumeClient, SupportsConsumeRequest
 
 __all__: Sequence[str] = ("ConsumerDecorator",)
 
@@ -14,14 +14,14 @@ T = TypeVar("T", Callable, Type[Service])
 
 @dataclass
 class ConsumerDecorator(Decorator[T]):
-    consumer: Union[SupportsRequestConsumer, SupportsClientConsumer]
+    consumer: Union[SupportsConsumeRequest, SupportsConsumeClient]
 
     def __call__(self, target: T, /) -> T:
         if isinstance(target, type):
             if not issubclass(target, Service):
                 raise CompositionError(f"Target class is not a subclass of {Service}")
 
-            if not isinstance(self.consumer, SupportsClientConsumer):
+            if not isinstance(self.consumer, SupportsConsumeClient):
                 raise CompositionError(
                     f"Consumer {type(self.consumer).__name__!r} does not support consumption"
                     f" of type {Service}"
@@ -31,7 +31,7 @@ class ConsumerDecorator(Decorator[T]):
 
             self.consumer.consume_client(client)
         elif callable(target):
-            if isinstance(self.consumer, SupportsRequestConsumer):
+            if isinstance(self.consumer, SupportsConsumeRequest):
                 pre_request: PreRequest = get_operation(target).request_options
 
                 self.consumer.consume_request(pre_request)
