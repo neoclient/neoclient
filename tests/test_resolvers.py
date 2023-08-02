1c612bef027236d1be4a31ab2b674a722f56562f
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2023-08-02 22:59:24 +0100

    tests/test_resolvers: test resolve_request methods of resolvers

--- tests/test_resolvers.py
+++ tests/test_resolvers.py
@@ -1,6 +1,6 @@
 from httpx import Cookies, Headers, QueryParams
 
-from neoclient.models import Response, State
+from neoclient.models import PreRequest, Response, State
 from neoclient.resolvers import (
     BodyResolver,
     CookieResolver,
@@ -15,6 +15,16 @@ from neoclient.resolvers import (
 from . import utils
 
 
+def test_QueryResolver_resolve_request() -> None:
+    request_with_param: PreRequest = utils.build_pre_request(
+        params={"name": "sam"},
+    )
+    request_without_param: PreRequest = utils.build_pre_request()
+
+    assert QueryResolver("name").resolve_request(request_with_param) == ["sam"]
+    assert QueryResolver("name").resolve_request(request_without_param) is None
+
+
 def test_QueryResolver_resolve_response() -> None:
     response_with_param: Response = utils.build_response(
         request=utils.build_request(
@@ -27,6 +37,16 @@ def test_QueryResolver_resolve_response() -> None:
     assert QueryResolver("name").resolve_response(response_without_param) is None
 
 
+def test_HeaderResolver_resolve_request() -> None:
+    request_with_header: PreRequest = utils.build_pre_request(
+        headers={"name": "sam"},
+    )
+    request_without_header: PreRequest = utils.build_pre_request()
+
+    assert HeaderResolver("name").resolve_request(request_with_header) == ["sam"]
+    assert HeaderResolver("name").resolve_request(request_without_header) is None
+
+
 def test_HeaderResolver_resolve_response() -> None:
     response_with_header: Response = utils.build_response(
         headers={"name": "sam"},
@@ -35,6 +55,16 @@ def test_HeaderResolver_resolve_response() -> None:
 
     assert HeaderResolver("name").resolve_response(response_with_header) == ["sam"]
     assert HeaderResolver("name").resolve_response(response_without_header) is None
+
+
+def test_CookieResolver_resolve_request() -> None:
+    request_with_cookie: PreRequest = utils.build_pre_request(
+        cookies={"name": "sam"},
+    )
+    request_without_cookie: PreRequest = utils.build_pre_request()
+
+    assert CookieResolver("name").resolve_request(request_with_cookie) == "sam"
+    assert CookieResolver("name").resolve_request(request_without_cookie) is None
 
 
 def test_CookieResolver_resolve_response() -> None:
@@ -47,6 +77,14 @@ def test_CookieResolver_resolve_response() -> None:
     assert CookieResolver("name").resolve_response(response_without_cookie) is None
 
 
+def test_QueriesResolver_resolve_request() -> None:
+    request: PreRequest = utils.build_pre_request(
+        params={"name": "sam"},
+    )
+
+    assert QueriesResolver().resolve_request(request) == QueryParams({"name": "sam"})
+
+
 def test_QueriesResolver_resolve_response() -> None:
     response: Response = utils.build_response(
         request=utils.build_request(
@@ -57,12 +95,28 @@ def test_QueriesResolver_resolve_response() -> None:
     assert QueriesResolver().resolve_response(response) == QueryParams({"name": "sam"})
 
 
+def test_HeadersResolver_resolve_request() -> None:
+    request: PreRequest = utils.build_pre_request(
+        headers={"name": "sam"},
+    )
+
+    assert HeadersResolver().resolve_request(request) == Headers({"name": "sam"})
+
+
 def test_HeadersResolver_resolve_response() -> None:
     response: Response = utils.build_response(
         headers={"name": "sam"},
     )
 
     assert HeadersResolver().resolve_response(response) == Headers({"name": "sam"})
+
+
+def test_CookiesResolver_resolve_request() -> None:
+    request: PreRequest = utils.build_pre_request(
+        cookies={"name": "sam"},
+    )
+
+    assert CookiesResolver().resolve_request(request) == Cookies({"name": "sam"})
 
 
 def test_CookiesResolver_resolve_response() -> None:
@@ -81,7 +135,17 @@ def test_BodyResolver() -> None:
     assert BodyResolver()(response) == {"name": "sam"}
 
 
-def test_StateResolver() -> None:
+def test_StateResolver_resolve_request() -> None:
+    message: str = "Hello, World!"
+
+    request: PreRequest = utils.build_pre_request(
+        state=State({"message": message}),
+    )
+
+    assert StateResolver("message").resolve_request(request) == message
+
+
+def test_StateResolver_resolve_response() -> None:
     message: str = "Hello, World!"
 
     response: Response = utils.build_response(
--- tests/utils.py
+++ tests/utils.py
@@ -2,12 +2,21 @@ from http import HTTPStatus
 from typing import Sequence
 
 from neoclient.enums import HTTPMethod
-from neoclient.models import Request, Response
+from neoclient.models import PreRequest, Request, Response
 
 __all__: Sequence[str] = (
     "build_request",
     "build_response",
 )
+
+
+def build_pre_request(
+    *,
+    method: str = HTTPMethod.GET,
+    url: str = "https://foo.com/",
+    **kwargs,
+) -> PreRequest:
+    return PreRequest(method, url, **kwargs)
 
 
 def build_request(
