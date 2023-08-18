6596accbe0e6d8460591401d0fc4b67c377658a6
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2023-08-18 21:45:18 +0100

    [#161] Unit test param functions: AllRequestState, AllResponseState, AllState

---
+++ tests/test_param_functions.py
@@ -0,0 +1,14 @@
+from neoclient.param_functions import AllRequestState, AllResponseState, AllState
+from neoclient.params import AllRequestStateParameter
+
+
+def test_AllRequestState() -> None:
+    assert AllRequestState() == AllRequestStateParameter()
+
+
+def test_AllResponseState() -> None:
+    assert AllResponseState() == AllResponseStateParameter()
+
+
+def test_AllState() -> None:
+    assert AllState() == AllStateParameter()
