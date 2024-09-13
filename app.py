f4097fc95a059764956d11daf5f256b72a52cfd8
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2024-09-13 22:08:09 +0100

    Refactor response decorators

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
+import httpx
 from neoclient import NeoClient, header, param, params
-from neoclient.decorators import set_header, add_header, add_param, set_param, cookie, headers, cookies
+from neoclient.decorators import (
+    set_header,
+    add_header,
+    add_param,
+    set_param,
+    cookie,
+    headers,
+    cookies,
+    response,
+)
+from neoclient.param_functions import Headers
 
 client = NeoClient(
     "https://httpbin.org/",
     headers={"name": "client"},
 )
+
+
+def response_headers(headers: httpx.Headers = Headers(), /) -> httpx.Headers:
+    return headers
+
 
 # @cookie("food", "chips", domain="b.com")
 # @cookie("food", "pizza", domain="a.com")
@@ -14,6 +30,7 @@ client = NeoClient(
 # @header("name", "a")
 # @headers({"name": "bob"})
 # @headers({"name": "sam"})
+@response(response_headers)
 @cookies({"name": "bob"})
 @cookies({"name": "sam"})
 @client.get("/get")
--- neoclient/decorators/__init__.py
+++ neoclient/decorators/__init__.py
@@ -3,10 +3,10 @@ from ._client import *
 from ._common import *
 from ._headers import *
 from ._middleware import *
+from ._response import *
 from ._utils import *
 
 from .old_api import CommonDecorator, OperationDecorator, ServiceDecorator
 from .operation import content, data, files, json, mount, path, path_params
 from .request import delete, get, head, options, patch, post, put, request
-from .response import response
 from .service import service
