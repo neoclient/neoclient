6af97d903f2cd9c8940eab3d323f1888c50b6001
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2024-09-14 23:47:42 +0100

    Create v1 api for RequestOpts in app file

--- app.request-options.py
+++ app.request-options.py
@@ -2,26 +2,20 @@ from dataclasses import dataclass
 from typing import Any, Optional, Union
 
 import httpx
-from httpx import Client, Cookies, Headers, QueryParams, Timeout
+from httpx import Cookies, Headers, QueryParams, Timeout
 from httpx._config import DEFAULT_TIMEOUT_CONFIG
 from httpx._types import (
-    AsyncByteStream,
     AuthTypes,
-    CertTypes,
     CookieTypes,
     HeaderTypes,
-    ProxiesTypes,
     QueryParamTypes,
     RequestContent,
     RequestData,
     RequestExtensions,
     RequestFiles,
-    SyncByteStream,
     TimeoutTypes,
     URLTypes,
-    VerifyTypes,
 )
-from httpx._client import UseClientDefault, USE_CLIENT_DEFAULT
 from httpx._urls import URL
 
 # TEMP
@@ -31,8 +25,7 @@ httpx.Client.request
 
 MethodTypes = Union[str, bytes]
 
-# Request, RequestMeta, RequestOpts, RequestOptions
-# PreRequest, ...
+
 @dataclass
 class RequestOpts:
     method: str
@@ -44,17 +37,10 @@ class RequestOpts:
     data: Optional[RequestData]
     files: Optional[RequestFiles]
     json: Optional[Any]
-    # auth: Optional[AuthTypes]
-    timeout: Optional[Timeout]
+    auth: Optional[AuthTypes]
+    follow_redirects: bool
+    timeout: Timeout
     extensions: RequestExtensions
-    # Sending: <-- include
-    #   auth
-    #   follow_redirects
-    # Client:
-    #   verify
-    #   cert
-    #   trust_env
-    #   proxies
 
     def __init__(
         self,
@@ -68,8 +54,9 @@ class RequestOpts:
         data: Optional[RequestData] = None,
         files: Optional[RequestFiles] = None,
         json: Optional[Any] = None,
-        # auth: Optional[AuthTypes] = None,
-        timeout: Union[TimeoutTypes, UseClientDefault] = USE_CLIENT_DEFAULT,
+        auth: Optional[AuthTypes] = None,
+        follow_redirects: bool = False,
+        timeout: TimeoutTypes = DEFAULT_TIMEOUT_CONFIG,
         extensions: Optional[RequestExtensions] = None,
     ) -> None:
         self.method = (
@@ -85,8 +72,9 @@ class RequestOpts:
         self.data = data
         self.files = files
         self.json = json
-        # self.auth = auth
-        self.timeout = None if timeout is isinstance(timeout, UseClientDefault) else Timeout(timeout)
+        self.auth = auth
+        self.follow_redirects = follow_redirects
+        self.timeout = Timeout(timeout)
         self.extensions = extensions if extensions is not None else {}
 
     def __repr__(self) -> str:
@@ -94,67 +82,5 @@ class RequestOpts:
         url = str(self.url)
         return f"<{class_name}({self.method!r}, {url!r})>"
 
-    # def build(self) -> httpx.Request:
-    #     raise NotImplementedError
-    
-    # def send(self, client: Optional[Client] = None) -> httpx.Request:
-    #     raise NotImplementedError
-
 
 r: RequestOpts = RequestOpts("GET", "/")
-
-
-# httpx.request(...)
-# @dataclass
-# class Request1:
-    # method: str
-    # url: URLTypes
-    # params: Optional[QueryParamTypes] = None
-    # content: Optional[RequestContent] = None
-    # data: Optional[RequestData] = None
-    # files: Optional[RequestFiles] = None
-    # json: Optional[Any] = None
-    # headers: Optional[HeaderTypes] = None
-    # cookies: Optional[CookieTypes] = None
-    # auth: Optional[AuthTypes] = None
-    # proxies: Optional[ProxiesTypes] = None
-    # timeout: TimeoutTypes = DEFAULT_TIMEOUT_CONFIG
-    # Sending:
-    # follow_redirects: bool = False
-    # Client:
-    # verify: VerifyTypes = True
-    # cert: Optional[CertTypes] = None
-    # trust_env: bool = True
-
-
-# httpx.Request
-# @dataclass
-# class Request2:
-    # method: Union[str, bytes]
-    # url: Union["URL", str]
-    # params: Optional[QueryParamTypes] = None
-    # headers: Optional[HeaderTypes] = None
-    # cookies: Optional[CookieTypes] = None
-    # content: Optional[RequestContent] = None
-    # data: Optional[RequestData] = None
-    # files: Optional[RequestFiles] = None
-    # json: Optional[Any] = None
-    # stream: Union[SyncByteStream, AsyncByteStream, None] = None
-    # extensions: Optional[RequestExtensions] = None
-
-
-# httpx.Client.request(...)
-# class Request3:
-    # method: str
-    # url: URLTypes
-    # content: Optional[RequestContent] = None
-    # data: Optional[RequestData] = None
-    # files: Optional[RequestFiles] = None
-    # json: Optional[Any] = None
-    # params: Optional[QueryParamTypes] = None
-    # headers: Optional[HeaderTypes] = None
-    # cookies: Optional[CookieTypes] = None
-    # auth: Union[AuthTypes, UseClientDefault, None] = USE_CLIENT_DEFAULT
-    # follow_redirects: Union[bool, UseClientDefault] = USE_CLIENT_DEFAULT
-    # timeout: Union[TimeoutTypes, UseClientDefault] = USE_CLIENT_DEFAULT
-    # extensions: Optional[RequestExtensions] = None
