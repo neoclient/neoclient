19d4466df9398ae946137ef72e259625145f4b2c
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2024-09-21 00:05:16 +0100

    Implement dependency 'profiles' (kudos to Spring)

--- app.di.2.py
+++ app.di.2.py
@@ -1,23 +1,55 @@
-from typing import Any
+import inspect
+from typing import Annotated, Any, Callable, Optional
 
 from di import Container, bind_by_type
 from di.api.dependencies import DependentBase
-from di.dependent import Dependent
+from di.dependent import Dependent, Marker
 from di.executors import SyncExecutor
 from httpx import Headers, Request, Response
 
 request = Request("GET", "/", headers={"origin": "Request!"})
 response = Response(200, headers={"origin": "Response!"})
 
+request_container = Container()
+request_container.bind(bind_by_type(Dependent(Request, wire=False), Request))
+request_container.bind(bind_by_type(Dependent(Response, wire=False), Response))
 
+response_container = Container()
+response_container.bind(bind_by_type(Dependent(Request, wire=False), Request))
+response_container.bind(bind_by_type(Dependent(Response, wire=False), Response))
+
+
+def dependency(func: Optional[Callable] = None, /, *, profile: Optional[str] = None):
+    containers = {"request": request_container, "response": response_container}
+    use_containers = (
+        (containers[profile],) if profile is not None else containers.values()
+    )
+
+    def decorate(func):
+        return_annotation = inspect.signature(func).return_annotation
+
+        for container in use_containers:
+            container.bind(bind_by_type(Dependent(func), return_annotation))
+
+        return func
+
+    if func is None:
+        return decorate
+    else:
+        return decorate(func)
+
+
+@dependency(profile="response")
 def extract_request(response: Response, /) -> Request:
     return response.request
 
 
+@dependency(profile="request")
 def extract_headers_from_request(request: Request, /) -> Headers:
     return request.headers
 
 
+@dependency(profile="response")
 def extract_headers_from_response(response: Response, /) -> Headers:
     return response.headers
 
@@ -26,29 +58,10 @@ def extract_origin(headers: Headers, /) -> str:
     return headers["origin"]
 
 
-def build_request_container() -> Container:
-    container = Container()
-
-    container.bind(bind_by_type(Dependent(Request, wire=False), Request))
-
-    container.bind(bind_by_type(Dependent(extract_headers_from_request), Headers))
-
-    return container
+def my_dependency(origin: Annotated[str, Marker(extract_origin)], /) -> str:
+    return f"Origin: {origin}"
 
 
-def build_response_container() -> Container:
-    container = Container()
-
-    container.bind(bind_by_type(Dependent(Response, wire=False), Response))
-    container.bind(bind_by_type(Dependent(extract_request), Request))
-
-    container.bind(bind_by_type(Dependent(extract_headers_from_response), Headers))
-
-    return container
-
-
-request_container = build_request_container()
-response_container = build_response_container()
 executor = SyncExecutor()
 
 
@@ -74,5 +87,5 @@ def solve_from_response(dependent: DependentBase[Any], /) -> str:
         )
 
 
-o1 = solve_from_request(extract_origin)
-o2 = solve_from_response(extract_origin)
+o1 = solve_from_request(my_dependency)
+o2 = solve_from_response(my_dependency)
