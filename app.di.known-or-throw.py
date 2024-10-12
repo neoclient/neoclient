a6b0084fcb97762ae290128b6fb77e2767135a5a
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2024-10-12 17:57:13 +0100

    Working PoC of throwing a wiring exception instead of guessing

---
+++ app.di.known-or-throw.py
@@ -0,0 +1,112 @@
+import inspect
+from typing import (
+    Any,
+    Callable,
+    MutableMapping,
+    MutableSequence,
+    NoReturn,
+    Optional,
+    TypeVar,
+)
+
+from di import Container
+from di.api.dependencies import DependentBase
+from di.api.providers import DependencyProviderType
+from di.dependent import Dependent
+from di.executors import SyncExecutor
+from httpx import Headers, QueryParams
+
+from neoclient.models import RequestOpts
+
+C = TypeVar("C", bound=Callable[..., Any])
+
+DEPENDENCIES: MutableMapping[type, MutableSequence[DependencyProviderType[type]]] = {}
+
+
+def not_wirable() -> NoReturn:
+    raise Exception("Not wirable")
+
+
+def bean(func: C, /) -> C:
+    dependency: Any = inspect.signature(func).return_annotation
+
+    assert isinstance(dependency, type)
+
+    DEPENDENCIES.setdefault(dependency, []).append(func)
+
+    return func
+
+
+@bean
+def extract_headers_from_request(request: RequestOpts, /) -> Headers:
+    return request.headers
+
+
+container = Container()
+
+# dependency: type
+# providers: Sequence[DependencyProviderType[type]]
+# for dependency, providers in DEPENDENCIES.items():
+#     if len(providers) != 1:
+#         raise NotImplementedError  # TODO
+
+#     provider = providers[0]
+
+#     container.bind(bind_by_type(Dependent(provider), dependency))
+
+# TEMP
+# container.bind(bind_by_type(Dependent(RequestOpts, wire=False), RequestOpts))
+
+
+def _bind_hook(
+    param: Optional[inspect.Parameter], dependent: DependentBase[Any]
+) -> Optional[DependentBase[Any]]:
+    print("_bind_hook", repr(param), dependent)
+
+    if param is None:
+        return None
+
+    if not isinstance(param.annotation, type):
+        raise NotImplementedError  # TODO
+    
+    # TEMP
+    if param.annotation is RequestOpts:
+        return Dependent(RequestOpts, wire=False)
+
+    dependency: type = param.annotation
+
+    # TODO: Improve this check (allow subclasses)
+    if dependency in DEPENDENCIES:
+        providers = DEPENDENCIES[dependency]
+
+        if len(providers) != 1:
+            raise NotImplementedError  # TODO
+
+        return Dependent(providers[0])
+        # return None
+
+    raise Exception(f"No provider registered for {param.annotation!r}")
+
+
+# TEMP
+container.bind(_bind_hook)
+
+
+def my_dependency(params: QueryParams, /):
+    return params
+
+
+executor = SyncExecutor()
+solved = container.solve(
+    Dependent(my_dependency),
+    scopes=(None,),
+)
+
+with container.enter_scope(None) as state:
+    d = solved.execute_sync(
+        executor=executor,
+        state=state,
+        values={
+            RequestOpts: RequestOpts("GET", "/", headers={"x-name": "bob"})
+        },
+    )
